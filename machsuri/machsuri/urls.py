ba989a3ffa9f7f71a26cf017d5a67ed49dc2d501
Byungsoo Kang <briankang0314@gmail.com>
2024-04-18 14:21:13 +0900

    modified:   machsuri/machsuri/urls.py and new file:   machsuri/machsuri/templates/home.html

--- machsuri/machsuri/urls.py
+++ machsuri/machsuri/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('machsuri.users.urls')),
+    # Direct the root URL to a simple home page view for now
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
